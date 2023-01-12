26c954a41f783c5e32471590dc60951b536eca62
AnlSakar <anilsakar01@gmail.com>
2023-01-12 23:55:26 +0300

    Add project files.

---
+++ TraversalApiProject/DAL/Context/VisitorContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TraversalApiProject.DAL.Entities;
+
+namespace TraversalApiProject.DAL.Context
+{
+    public class VisitorContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-KPOO71A\\SQLEXPRESS;initial catalog=TraversalDbApi;integrated security=true;");
+
+
+
+        }
+
+        public DbSet<Visitor> visitors { get; set; }
+    }
+}
